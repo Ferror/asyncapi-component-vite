84cc33f1b1d23ca992729d880d4882b8b4f446e3
Ferror <zmalcherczyk@gmail.com>
2023-02-24 13:47:41 +0100

    install asyncapi

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import * as React from "react";
+import { render } from "react-dom";
+import AsyncApiComponent, { ConfigInterface } from "@asyncapi/react-component";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const schema = `
+asyncapi: '2.0.0'
+info:
+  title: Example
+  version: '0.1.0'
+channels:
+  example-channel:
+    subscribe:
+      message:
+        payload:
+          type: object
+          properties:
+            exampleField:
+              type: string
+            exampleNumber:
+              type: number
+            exampleDate:
+              type: string
+              format: date-time
+`;
+
+const config: ConfigInterface = {
+    schemaID: 'custom-spec',
+    show: {
+        operations: false,
+        errors: false,
+    },
+};
+
+const App = () => <AsyncApiComponent schema={schema} config={config} />;
+
+render(<App />, document.getElementById("root"));
